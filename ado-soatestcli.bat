3fe2101f771ecb1d610257065b618a2b8f251fa4
gtrofimov <gtrofimov@parasoft.com>
2022-05-18 22:19:24 -0700

    Update ado-soatestcli.bat

--- ado-soatestcli.bat
+++ ado-soatestcli.bat
@@ -6,7 +6,7 @@ set "SOATEST_WORKSPACE=C:\Parasoft\workspace\soavirt\demo20222202"
 "%SOATEST_EXE%\soatestcli.exe" -data %SOATEST_WORKSPACE% -import %cd%\%PROJECT_NAME%
 
 :: RUN FUNCTIONAL TESTS WITH COV
-"%SOATEST_EXE%\soatestcli.exe" -data %SOATEST_WORKSPACE% -resource %PROJECT_NAME% -config "user://Example Configuration" -report %SOATEST_WORKSPACE%\report -publish -localsettings %SOATEST_WORKSPACE%\soatest.properties
+"%SOATEST_EXE%\soatestcli.exe" -data %SOATEST_WORKSPACE% -resource %PROJECT_NAME% -config "user://Example Configuration" -report %cd%\report -localsettings %SOATEST_WORKSPACE%\soatest.properties
 
 :: PUBLISH
 ::jtestcli -staticcoverage "%DEPLOY_HOME%\monitor\static_coverage.xml" -runtimecoverage "%DEPLOY_HOME%\monitor\runtime_coverage\functional" -config "builtin://Calculate Application Coverage" -publish -property report.coverage.images="Default Project";"Default Project_Functional Test" -property session.tag="Functional Test"
